246ec0d74f312da38cac39da7ea7e799bf14bfeb
claudio.procida <claudio.procida@ccf2457b-044c-0410-8c3d-51462c4d707e>
2008-04-29 18:49:13 +0000

    Moved the KML category to NSHost as it is a better place for it.

--- trunk/Source/AppController.m
+++ trunk/Source/AppController.m
@@ -6,17 +6,17 @@
 //  Copyright 2008 Claudio Procida - Emeraldion Lodge. All rights reserved.
 //
 //
-//	Licensed under the Apache License, Version 2.0 (the "License");
-//	you may not use this file except in compliance with the License.
-//	You may obtain a copy of the License at
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
-//	http://www.apache.org/licenses/LICENSE-2.0
+//  http://www.apache.org/licenses/LICENSE-2.0
 //
-//	Unless required by applicable law or agreed to in writing, software
-//	distributed under the License is distributed on an "AS IS" BASIS,
-//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//	See the License for the specific language governing permissions and
-//	limitations under the License.
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
 #import "AppController.h"
 #import "HostIPLookupResult.h"
@@ -32,10 +32,25 @@
 
 - (IBAction)lookupGeodata:(id)sender
 {
-	NSString *geodata = [[NSHost hostWithName:[addrField stringValue]] geodata];
+	NSHost *host = [NSHost hostWithName:[addrField stringValue]];
+	NSString *geodata = [host geodata];
 	// Same as:
 	// NSString *geodata = [HostIPLookup geodataForHostname:[addrField stringValue]];
 	[geodataField setStringValue:geodata];
+}
+
+- (IBAction)generateKML:(id)sender
+{
+	NSHost *host = [NSHost hostWithName:[addrField stringValue]];
+	NSString *KMLrepr = [host KMLRepresentation];
+	
+	NSSavePanel *savePanel = [NSSavePanel savePanel];
+	if ([savePanel runModalForDirectory:@""
+								   file:@"Untitled.kml"] == NSFileHandlingPanelOKButton)
+	{
+		[KMLrepr writeToFile:[savePanel filename]
+				  atomically:YES];
+	}
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notif
--- trunk/Source/NSHost+HostIPLookup.m
+++ trunk/Source/NSHost+HostIPLookup.m
@@ -6,17 +6,17 @@
 //  Copyright 2008 Claudio Procida - Emeraldion Lodge. All rights reserved.
 //
 //
-//	Licensed under the Apache License, Version 2.0 (the "License");
-//	you may not use this file except in compliance with the License.
-//	You may obtain a copy of the License at
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
-//	http://www.apache.org/licenses/LICENSE-2.0
+//  http://www.apache.org/licenses/LICENSE-2.0
 //
-//	Unless required by applicable law or agreed to in writing, software
-//	distributed under the License is distributed on an "AS IS" BASIS,
-//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//	See the License for the specific language governing permissions and
-//	limitations under the License.
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
 
 #import "NSHost+HostIPLookup.h"
@@ -29,6 +29,9 @@ static BOOL NSHost_HostIPLookupGeolocationCacheEnabled = YES;
 @interface NSHost (HostIPLookup_Private)
 
 - (HostIPLookupResult *)_lookupAndCache;
++ (NSString *)_KMLHeader:(NSString *)title;
++ (NSString *)_KMLFooter;
+- (NSString *)_KMLPlacemark;
 
 @end
 
@@ -87,6 +90,36 @@ static BOOL NSHost_HostIPLookupGeolocationCacheEnabled = YES;
 
 @end
 
+@implementation NSHost (KML)
+
+- (NSString *)KMLRepresentation
+{
+	return [NSString stringWithFormat:@"%@\n%@\n%@",
+		[[self class] _KMLHeader:[self name]],
+		[self _KMLPlacemark],
+		[[self class] _KMLFooter]];
+}
+
++ (NSString *)KMLRepresentationForHosts:(NSArray *)hosts
+{
+	NSMutableString *buf = [NSMutableString string];
+	[buf appendString:[[self class] _KMLHeader:NSLocalizedString(@"HostIPLookup generated KML file",
+																 @"HostIPLookup generated KML file")]];
+	unsigned int cnt = [hosts count], i;
+	NSHost *host;
+	for (i = 0; i < cnt; i++)
+	{
+		host = (NSHost *)[hosts objectAtIndex:i];
+		[buf appendString:[host _KMLPlacemark]];
+	}
+	
+	[buf appendString:[[self class] _KMLFooter]];
+	
+	return [[buf copy] autorelease];
+}
+
+@end
+
 @implementation NSHost (HostIPLookup_Private)
 
 - (HostIPLookupResult *)_lookupAndCache
@@ -110,6 +143,84 @@ static BOOL NSHost_HostIPLookupGeolocationCacheEnabled = YES;
 	{
 		return [HostIPLookup lookupAddress:[self address]];
 	}
+}
+
+- (NSString *)_KMLPlacemark
+{
+	NSString *placemarkFormat = @"<Placemark>\n\
+	<name>%@</name>\n\
+	<visibility>1</visibility>\n\
+	<Snippet>\n\
+	<![CDATA[\n\
+		%@\n\
+		]]>\n\
+	</Snippet>\n\
+	<description>\n\
+	<![CDATA[\n\
+		<h3>%@</h3>\n\
+		<table>\n\
+		<tr>\n\
+		<td>Country</td>\n\
+		<td>%@</td>\n\
+		</tr>\n\
+		<tr>\n\
+		<td>Country Code</td>\n\
+		<td>%@</td>\n\
+		</tr>\n\
+		<tr>\n\
+		<td>City</td>\n\
+		<td>%@</td>\n\
+		</tr>\n\
+		<tr>\n\
+		<td>Latitude</td>\n\
+		<td>%.4f</td>\n\
+		</tr>\n\
+		<tr>\n\
+		<td>Longitude</td>\n\
+		<td>%.4f</td>\n\
+		</tr>\n\
+		<tr>\n\
+		<td>IP Address</td>\n\
+		<td>%@</td>\n\
+		</tr>\n\
+		</table>\n\
+		]]>\n\
+	</description>\n\
+	<Point>\n\
+	<extrude>1</extrude>\n\
+	<altitudeMode>relativeToGround</altitudeMode>\n\
+	<coordinates>%.4f,%.4f,0</coordinates>\n\
+	</Point>\n\
+	</Placemark>";
+	
+	return [NSString stringWithFormat:placemarkFormat,
+		[self name],
+		[self address],
+		[self name],
+		[self country],
+		[self countryCode],
+		[self city],
+		[self latitude],
+		[self longitude],
+		[self address],
+		[self longitude],
+		[self latitude]];	
+}
+
++ (NSString *)_KMLHeader:(NSString *)title
+{
+	return [NSString stringWithFormat:@"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\
+	<kml xmlns=\"http://earth.google.com/kml/2.0\">\n\
+		<Document>\n\
+			<name>%@</name>\n\
+			<open>1</open>",
+		title];
+}
+
++ (NSString *)_KMLFooter
+{
+	return @"</Document>\n\
+</kml>";
 }
 
 @end
